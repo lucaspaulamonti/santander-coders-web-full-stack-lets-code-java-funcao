f667d589189fdca829468b9a1d5ebfba688d2837
Lucas Monti <108158100+MontiLucas@users.noreply.github.com>
2022-09-18 15:30:29 -0300

    Initial commit

---
+++ funcao/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Crie uma função que imprima uma variável.
+        String nomeOriginal = "Lucas";
+        saudacao(nomeOriginal);
+    }
+    public static void saudacao(String nomeMetodo){
+        System.out.println("Hello, " + nomeMetodo + "!");
+    }
+}
